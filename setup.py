aa1a90a5a6bdca656d3ba31dd62ab9218b126862
Dolph Mathews <dolph.mathews@gmail.com>
2013-05-30 14:05:59 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     name='next-review',
     description='Start your next gerrit code review without any hassle.',
-    version='1.0.4',
+    version='1.1.0',
     scripts=['next_review.py'],
     install_requires=['paramiko'],
     py_modules=['next_review'],

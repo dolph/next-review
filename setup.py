0836be9501dad77810a5f535f422fbbb81a9f3fa
Dolph Mathews <dolph.mathews@gmail.com>
2013-05-30 14:51:50 -0500

    Added project metadata

--- setup.py
+++ setup.py
@@ -3,9 +3,19 @@ import setuptools
 
 setuptools.setup(
     name='next-review',
-    description='Start your next gerrit code review without any hassle.',
     version='1.1.0',
+    description='Start your next gerrit code review without any hassle.',
+    author='Dolph Mathews',
+    author_email='dolph.mathews@gmail.com',
+    url='http://github.com/dolph/next-review',
     scripts=['next_review.py'],
     install_requires=['paramiko'],
     py_modules=['next_review'],
-    entry_points={'console_scripts': ['next-review = next_review:cli']})
+    entry_points={'console_scripts': ['next-review = next_review:cli']},
+    classifiers=[
+        'Development Status :: 5 - Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+    ],
+}

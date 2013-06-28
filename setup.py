1f0f74778a98e6ba0732357864fff02637bc0574
Dolph Mathews <dolph.mathews@gmail.com>
2013-06-28 10:52:08 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='next-review',
-    version='1.2.0',
+    version='1.2.1',
     description='Start your next gerrit code review without any hassle.',
     author='Dolph Mathews',
     author_email='dolph.mathews@gmail.com',
